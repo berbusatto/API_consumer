6117f036244fb443bd23adebb0d6277a41fb96de
Bernardo Busatto <berbusatto@gmail.com>
2022-08-25 19:43:02 -0300

    atualizando pro aryon

---
+++ app.js
@@ -0,0 +1,9 @@
+$.ajax({
+    type: 'get',
+    url: "http://localhost:8000/hello-world?info=Odnumalo"
+}).done(function(data) { 
+    document.getElementById('developer').innerHTML = data.manager_developer
+    document.getElementById('version').innerHTML = data.version
+    document.getElementById('value-of-variable').innerHTML = data.value_of_variable
+    document.getElementById('company-site').innerHTML = data.website_company
+});    
