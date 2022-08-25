2c2c7a021deffc49591d3eb0a62c845e680ba819
Bernardo Busatto <berbusatto@gmail.com>
2022-08-25 20:29:15 -0300

    atualizando front

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-$.ajax({
-    type: 'get',
-    url: "http://localhost:8000/hello-world?info=Odnumalo"
-}).done(function(data) { 
-    document.getElementById('developer').innerHTML = data.manager_developer
-    document.getElementById('version').innerHTML = data.version
-    document.getElementById('value-of-variable').innerHTML = data.value_of_variable
-    document.getElementById('company-site').innerHTML = data.website_company
-});    
+function requestOfServer(){
+
+    $.ajax({
+        type: 'get',
+        url: "http://localhost:8000/hello-world?info=Odnumalo"
+    }).done(function(data) { 
+        document.getElementById('developer').innerHTML = data.manager_developer
+        document.getElementById('version').innerHTML = data.version
+        document.getElementById('value-of-variable').innerHTML = data.value_of_variable
+        document.getElementById('company-site').innerHTML = data.website_company
+
+        $("#show-values").css('display', 'block')
+
+
+    });
+}
