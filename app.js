3c097d92c51a1b31214d76e9848374b33c3209b8
Bernardo Busatto <berbusatto@gmail.com>
2022-09-22 23:12:47 -0300

    atualizando front

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
-function requestOfServer(){
+let a =1
+let b =2*a
 
+function requestOfServer() {
     $.ajax({
         type: 'get',
-        url: "http://localhost:8000/hello-world?info=Odnumalo"
+        url: "http://localhost:8000/hello-world?info=OlaMundo"
     }).done(function(data) { 
-        document.getElementById('developer').innerHTML = data.manager_developer
+        document.getElementById('developer').innerHTML = 
+data.name
+
         document.getElementById('version').innerHTML = data.version
-        document.getElementById('value-of-variable').innerHTML = data.value_of_variable
-        document.getElementById('company-site').innerHTML = data.website_company
+        document.getElementById('value-of-variable').innerHTML = data.value_of_variable_info
+        document.getElementById('company-site').innerHTML = data.web_site_company
 
         $("#show-values").css('display', 'block')
-
 
     });
 }
 
-function submitDataOfUser(){
+function submitDataOfUser() {
     const nameOfUser = $("#name").val()
-    const lastNameOfUser = $("#lastname").val()
-    const ageOfUser = $("#age").val()
+    const lasNameOfUser = $("#lastname").val()
+    const ageOfUser = $("#age").val()      
+    $.ajax({
+        "url": "http://localhost:8000/insert-data",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json; charset=UTF-8"
+        },
+        "data": JSON.stringify({
+            name:nameOfUser,
+            lastName:lasNameOfUser,
+            age:ageOfUser
+        }) 
+    }).done(function(response) {
+        
+        if (response.success) {
+            $("#success-record-msg").css('display', 'block')
+            $("#name").val('')
+            $("#lastname").val('')
+            $("#age").val('')
+        }
+    });  
+} 
 
-    console.log(nameOfUser)
-    console.log(lastNameOfUser)
-    console.log(ageOfUser)
+function closeSuccessMsg(){
+    // COM JQUERY
+    $('#success-record-msg').css('display', 'none')
+
+    // JS VANILLA
+    //document.getElementById('success-record-msg').style.display = 'none'
+
 }
